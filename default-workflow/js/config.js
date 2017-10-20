d0187b7f37d586cd4719caa482b0b80367a6a08c
scmwalters <michael.walters@shortcuts.com.au>
2017-10-20 10:21:41 +1000

    Added default-workflow site testing pending.

---
+++ default-workflow/js/config.js
@@ -0,0 +1,14 @@
+﻿define(function () {
+    return {
+        widgetBaseUri: 'd19ujuohqco9tx.cloudfront.net/webwidgets/7-14-0-39800',
+        apiUri: 'pos.shortcutssoftware.com/webapi',
+        apiVersion: '7.14.0',
+        // IMPORTANT
+        // In a production system, these should be provided by dynamic AJAX call, session cookie
+        // or other dynamic process rather than in static content.
+        consumerKey: '',
+        consumerSecret: '',
+        accessTokenKey: '',
+        accessTokenSecret: ''
+    };
+});
