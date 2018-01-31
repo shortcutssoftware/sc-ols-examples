c5cdeebf863d53fd765c265802ce7a62c96d17dd
Rob Barrett <rob.barrett@shortcuts.com.au>
2018-02-01 09:04:15 +1000

    API usage examples

--- v2-examples/js/test/search.js
+++ v2-examples/js/test/search.js
@@ -1,61 +1,90 @@
 const expect = require('expect.js');
 const search = require('../src/search.js');
+const authenticate = require('../src/authenticate.js');
 
 describe('Search', function () {
     this.timeout(10000);
 
+    var sharedState = {
+        access_token: undefined
+    };
+
+    it('must authenticate', function (done) {
+        authenticate.authenticate(function (err, result) {
+            if (err) {
+                done(err);
+                return;
+            }
+
+            expect(result.status).to.eql(200);
+            expect(result.content.access_token).to.not.be(undefined);
+            sharedState.access_token = result.content.access_token;
+            done();
+        })
+    });
+
     describe('byServiceCategoryName()', function () {
         it('should be able to search for availabilities Beauty services', function (done) {
-            search.byServiceCategoryName('Beauty', function (err, result) {
-                if (err) {
-                    done(err);
-                    return;
-                }
+            search.byServiceCategoryName(
+                sharedState.access_token,
+                'Beauty',
+                function (err, result) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
 
-                expect(result.status).to.eql(200);
-                expect(result.content.available_appointments).to.not.be(undefined);
-                expect(result.content.available_appointments).to.be.an('array');
-                expect(result.content.available_appointments).to.not.be.empty();
+                    expect(result.status).to.eql(200);
+                    expect(result.content.available_appointments).to.not.be(undefined);
+                    expect(result.content.available_appointments).to.be.an('array');
+                    expect(result.content.available_appointments).to.not.be.empty();
 
-                done();
-            })
+                    done();
+                })
         })
     });
 
     describe('byServiceName()', function () {
         it('should be able to search for availabilities of the Wax Leg service', function (done) {
-            search.byServiceName('Wax Leg', function (err, result) {
-                if (err) {
-                    done(err);
-                    return;
-                }
+            search.byServiceName(
+                sharedState.access_token,
+                'Wax Leg',
+                function (err, result) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
 
-                expect(result.status).to.eql(200);
-                expect(result.content.available_appointments).to.not.be(undefined);
-                expect(result.content.available_appointments).to.be.an('array');
-                expect(result.content.available_appointments).to.not.be.empty();
+                    expect(result.status).to.eql(200);
+                    expect(result.content.available_appointments).to.not.be(undefined);
+                    expect(result.content.available_appointments).to.be.an('array');
+                    expect(result.content.available_appointments).to.not.be.empty();
 
-                done();
-            })
+                    done();
+                })
         })
     });
 
-    
+
     describe('byServiceAndEmployeeName()', function () {
         it('should be able to search for availabilities of the Wax Leg service with Wendy', function (done) {
-            search.byServiceAndEmployeeName('Wax Leg', 'Wendy', function (err, result) {
-                if (err) {
-                    done(err);
-                    return;
-                }
+            search.byServiceAndEmployeeName(
+                sharedState.access_token,
+                'Wax Leg',
+                'Wendy',
+                function (err, result) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
 
-                expect(result.status).to.eql(200);
-                expect(result.content.available_appointments).to.not.be(undefined);
-                expect(result.content.available_appointments).to.be.an('array');
-                expect(result.content.available_appointments).to.not.be.empty();
+                    expect(result.status).to.eql(200);
+                    expect(result.content.available_appointments).to.not.be(undefined);
+                    expect(result.content.available_appointments).to.be.an('array');
+                    expect(result.content.available_appointments).to.not.be.empty();
 
-                done();
-            })
+                    done();
+                })
         })
     });
 });
--- v2-examples/js/test/site.js
+++ v2-examples/js/test/site.js
@@ -2,137 +2,175 @@ const expect = require('expect.js');
 const config = require('../src/config.js');
 const site = require('../src/site.js');
 const url = require('url');
+const authenticate = require('../src/authenticate.js');
 
 describe('Site', function () {
     this.timeout(5000);
 
-    describe('retrieveEmployeeByEmployeeAlias', function() {
-        it('should be able to retrieve Wendy', function (done) {
-            site.retrieveEmployeeByEmployeeAlias('Wendy', function (err, result) {
-                if (err) {
-                    done(err);
-                    return;
-                }
+    var sharedState = {
+        access_token: undefined
+    };
 
-                expect(result.status).to.eql(200);
-                expect(result.content.employees).to.not.be(undefined);
-                expect(result.content.employees).to.be.an('array');
-                expect(result.content.employees).to.not.be.empty();
-                
-                done();
-            })
+    it('must authenticate', function (done) {
+        authenticate.authenticate(function (err, result) {
+            if (err) {
+                done(err);
+                return;
+            }
+
+            expect(result.status).to.eql(200);
+            expect(result.content.access_token).to.not.be(undefined);
+            sharedState.access_token = result.content.access_token;
+            done();
+        })
+    });
+
+    describe('retrieveEmployeeByEmployeeAlias', function () {
+        it('should be able to retrieve Wendy', function (done) {
+            site.retrieveEmployeeByEmployeeAlias(
+                sharedState.access_token,
+                'Wendy',
+                function (err, result) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    expect(result.status).to.eql(200);
+                    expect(result.content.employees).to.not.be(undefined);
+                    expect(result.content.employees).to.be.an('array');
+                    expect(result.content.employees).to.not.be.empty();
+
+                    done();
+                })
         })
     });
 
     describe('retrieveSiteDetails()', function () {
         it('should be able to retrieve site details', function (done) {
-            site.retrieveSiteDetails(function (err, result) {
-                if (err) {
-                    done(err);
-                    return;
-                }
+            site.retrieveSiteDetails(
+                sharedState.access_token,
+                function (err, result) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
 
-                expect(result.status).to.eql(200);
-                expect(result.content).to.not.be(undefined);
-                expect(result.content).to.be.an('object');
-                expect(result.content.href).to.not.be(undefined);
-                expect(result.content.href).to.be.an('string');
-                expect(result.content.href).to.eql(url.resolve(config.apiUri, 'site/' + config.siteId))
+                    expect(result.status).to.eql(200);
+                    expect(result.content).to.not.be(undefined);
+                    expect(result.content).to.be.an('object');
+                    expect(result.content.href).to.not.be(undefined);
+                    expect(result.content.href).to.be.an('string');
+                    expect(result.content.href).to.eql(url.resolve(config.apiUri, 'site/' + config.siteId))
 
-                done();
-            })
+                    done();
+                })
         })
     });
 
     describe('retrieveServiceCategories()', function () {
         it('should be able to retrieve service categories', function (done) {
-            site.retrieveServiceCategories(function (err, result) {
-                if (err) {
-                    done(err);
-                    return;
-                }
+            site.retrieveServiceCategories(
+                sharedState.access_token,
+                function (err, result) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
 
-                
-                expect(result.status).to.eql(200);
-                expect(result.content.service_categories).to.not.be(undefined);
-                expect(result.content.service_categories).to.be.an('array');
-                expect(result.content.service_categories).to.not.be.empty();
 
-                done();
-            })
+                    expect(result.status).to.eql(200);
+                    expect(result.content.service_categories).to.not.be(undefined);
+                    expect(result.content.service_categories).to.be.an('array');
+                    expect(result.content.service_categories).to.not.be.empty();
+
+                    done();
+                })
         })
     });
-    
+
     describe('retrieveServicesByServiceCategoryName()', function () {
         it('should be able to retrieve beauty services', function (done) {
-            site.retrieveServicesByServiceCategoryName('Beauty', function (err, result) {
-                if (err) {
-                    done(err);
-                    return;
-                }
+            site.retrieveServicesByServiceCategoryName(
+                sharedState.access_token,
+                'Beauty',
+                function (err, result) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
 
-                expect(result.status).to.eql(200);
-                expect(result.content.services).to.not.be(undefined);
-                expect(result.content.services).to.be.an('array');
-                expect(result.content.services).to.not.be.empty();
+                    expect(result.status).to.eql(200);
+                    expect(result.content.services).to.not.be(undefined);
+                    expect(result.content.services).to.be.an('array');
+                    expect(result.content.services).to.not.be.empty();
 
-                done();
-            })
+                    done();
+                })
         })
     });
-    
+
     describe('retrieveServicesByServiceName()', function () {
         it('should be able to retrieve wax leg service', function (done) {
-            site.retrieveServicesByServiceName('Wax Leg', function (err, result) {
-                if (err) {
-                    done(err);
-                    return;
-                }
+            site.retrieveServicesByServiceName(
+                sharedState.access_token,
+                'Wax Leg',
+                function (err, result) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
 
-                expect(result.status).to.eql(200);
-                expect(result.content.services).to.not.be(undefined);
-                expect(result.content.services).to.be.an('array');
-                expect(result.content.services).to.not.be.empty();
+                    expect(result.status).to.eql(200);
+                    expect(result.content.services).to.not.be(undefined);
+                    expect(result.content.services).to.be.an('array');
+                    expect(result.content.services).to.not.be.empty();
 
-                done();
-            })
+                    done();
+                })
         })
     });
 
-    
+
     describe('retrieveServiceEmployeePricing()', function () {
         it('should be able to retrieve employee pricing for the Wax Leg service.', function (done) {
-            site.retrieveServiceEmployeePricing('Wax Leg', function (err, result) {
-                if (err) {
-                    done(err);
-                    return;
-                }
+            site.retrieveServiceEmployeePricing(
+                sharedState.access_token,
+                'Wax Leg',
+                function (err, result) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
 
-                expect(result.status).to.eql(200);
-                expect(result.content.employees).to.not.be(undefined);
-                expect(result.content.employees).to.be.an('array');
-                expect(result.content.employees).to.not.be.empty();
+                    expect(result.status).to.eql(200);
+                    expect(result.content.employees).to.not.be(undefined);
+                    expect(result.content.employees).to.be.an('array');
+                    expect(result.content.employees).to.not.be.empty();
 
-                done();
-            })
+                    done();
+                })
         })
     });
 
-    describe('retrieveEmployeeServicePricing', function() {
+    describe('retrieveEmployeeServicePricing', function () {
         it('should be able to retrieve service pricing for employee with alias of Wendy', function (done) {
-            site.retrieveEmployeeServicePricing('Wendy', function (err, result) {
-                if (err) {
-                    done(err);
-                    return;
-                }
+            site.retrieveEmployeeServicePricing(
+                sharedState.access_token,
+                'Wendy',
+                function (err, result) {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
 
-                expect(result.status).to.eql(200);
-                expect(result.content.services).to.not.be(undefined);
-                expect(result.content.services).to.be.an('array');
-                expect(result.content.services).to.not.be.empty();
+                    expect(result.status).to.eql(200);
+                    expect(result.content.services).to.not.be(undefined);
+                    expect(result.content.services).to.be.an('array');
+                    expect(result.content.services).to.not.be.empty();
 
-                done();
-            })
+                    done();
+                })
         })
     });
 });
